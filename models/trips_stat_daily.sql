eb9e073b1cef66d7b1dc01197a67cefdad906de7
yansotnya@gmail.com <yansotnya@gmail.com>
2024-04-08 18:10:27 +0000

    create new scooters tables

---
+++ models/trips_stat_daily.sql
@@ -0,0 +1,8 @@
+select
+    date(started_at) as date,
+    count(started_at) as trips,
+    max(price) / 100 as max_price_rub,
+    avg(distance) / 1000 as avg_distance_km
+from scooters_raw.trips
+group by 1
+order by 1
