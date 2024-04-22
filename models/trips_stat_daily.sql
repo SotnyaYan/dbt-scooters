0c034474f5415c7fea082b3590467e13b00c1a24
yansotnya@gmail.com <yansotnya@gmail.com>
2024-04-22 05:55:00 +0000

    add new sql

--- models/trips_stat_daily.sql
+++ models/trips_stat_daily.sql
@@ -1,8 +1,12 @@
 select
-    date(started_at) as date,
-    count(started_at) as trips,
-    max(price) / 100 as max_price_rub,
-    avg(distance) / 1000 as avg_distance_km
-from scooters_raw.trips
-group by 1
-order by 1
+    "date",
+    count(*) as trips,
+    max(price_rub) as max_price_rub,
+    avg(distance_m) / 1000 as avg_distance_km,
+    avg(price_rub) / avg(duration_s) * 60 as avg_price_rub_per_min
+from
+    {{ ref("trips_prep") }}
+group by
+    1
+order by
+    1
